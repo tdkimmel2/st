c630e640d7513fe853622f824923d6669d41459b
tdkimmel2 <tdkimmel2@gmail.com>
2024-04-18 08:41:27 -0400

    Official Update: Unhide cursor on RIS (\033c)
    It is unclear if it's 'required' to do this on RIS, but it's useful when
    calling reset(1) after interactive programs have crashed and garbled up
    the screen.
    
    FWIW, other terminals do it as well (tested with XTerm, VTE, Kitty,
    Alacritty, Linux VT).

--- st.c
+++ st.c
@@ -2398,6 +2398,7 @@ eschandle(uchar ascii)
 		treset();
 		resettitle();
 		xloadcols();
+        xsetmode(0, MODE_HIDE);
 		break;
 	case '=': /* DECPAM -- Application keypad */
 		xsetmode(1, MODE_APPKEYPAD);
